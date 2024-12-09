fefe0866c525b5becfa353658087ce962ebc5f90
Grant Le Roux <cram42@users.noreply.github.com>
2024-12-09 11:28:41 +0800

    Bump version

--- src/plugin.py
+++ src/plugin.py
@@ -35,7 +35,7 @@ class FS25GamePlugin(IPluginGame):
         return "Cram42"
 
     def version(self) -> VersionInfo:
-        return VersionInfo("1.1.0")
+        return VersionInfo("1.2.0")
 
     def description(self) -> str:
         return "Game support for Farming Simulator 25."
